68267bb41f4eb796e06b7abc08ebb08451005aa8
Bhakti Daraniya <180540107035@darshan.ac.in>
2022-06-10 17:58:31 +0530

    redux-demo-code

--- App.js
+++ App.js
@@ -1,7 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import store from './src/redux/store';
+// import {bugAdded, bugRemoved, bugResolved} from './src/redux/actions';
+import * as actions from './src/redux/actions';
+import store from './src/redux/customStore';
 
 export default function App() {
+  
+  store.subscribe(() => {
+    console.log("Store changed");
+  })
+  store.dispatch(actions.bugAdded("Bug 1"));
+  // store.dispatch(actions.bugRemoved());
+  console.log(store.getState())
+  
+  // store.state =1 
+  // console.log(store );
+
+
+  // const unsubscribe = store.subscribe(() => {
+  //   console.log("Store changed", store.getState());
+  // })
+
+  // store.dispatch(bugAdded("Bug 1"));
+
+  // unsubscribe();
+
+  // store.dispatch(bugRemoved(1));
+
+  // store.dispatch(bugResolved(1));
+
+  // console.log(store)
+  // console.log(store.getState())
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ src/redux/actions.js
@@ -0,0 +1,24 @@
+import * as actionTypes from './actionTypes'
+
+const bugAdded = (description) => ({
+    type: actionTypes.BUG_ADDED,
+    payload : {
+      description
+    }
+})
+
+const bugRemoved = (id) => ({
+    type: actionTypes.BUG_REMOVED,
+    payload:{
+      id
+    }
+})
+
+const bugResolved = (id) => ({
+    type: actionTypes.BUG_RESOLVED,
+    payload : {
+        id
+    }
+})
+
+export {bugAdded,bugRemoved,bugResolved}
